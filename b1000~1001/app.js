9dfafea16b6c36c9695b7bf02072f3dbadcd4f16
codnr <wnd75913@gmail.com>
2022-03-14 03:05:33 +0900

    입출력과 사칙연산 1~10

---
+++ b1000~1001/app.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");// 줄바꿈으로 array를 나눔
+input = input[0];
+input = input.split(" ");
+
+const inputs = [];// number type으로 변환된 값이 들어갈 array
+
+for(i = 0; i < input.length; i++) { //input array의 length만큼 반복
+    const result = parseInt(input[i]); //result에 parseInt로 input array의 item들을 받아와 number type으로 변환해서 넣어줌
+    inputs.push(result); // 변환한 값들을 미리준비한 inputs array에 넣어줌
+}
+
+function solution(A, B) {
+    console.log(A + B);
+};
+
+solution(inputs[0], inputs[1]);
---
+++ b1008/app.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+input = input[0];
+input = input.split(" ");
+
+const inputs = [];
+
+for(i = 0; i < input.length; i++) {
+    const result = parseInt(input[i]);
+    inputs.push(result);
+};
+
+function solution(A, B) {
+    console.log(A / B);
+};
+
+solution(inputs[0], inputs[1]);
+console.log(input);
